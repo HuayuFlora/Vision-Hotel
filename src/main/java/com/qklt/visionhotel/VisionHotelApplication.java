ec4efdf7a3e960df6cb4098ab673cd559bde7165
MLP <1033430415@qq.com>
2019-11-18 22:15:50 +0800

    Vision-Hotel

---
+++ src/main/java/com/qklt/visionhotel/VisionHotelApplication.java
@@ -0,0 +1,13 @@
+package com.qklt.visionhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qklt/visionhotel/VisionHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qklt.visionhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
